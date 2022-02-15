2ab3bb3e5cfe07ba5e5ebbcdfda0cb6f0adc67fa
Fraser Dillon <fraserdillon050388@gmail.com>
2022-02-15 15:54:03 +0000

    enterprise abstract class initialised and constructor created

---
+++ src/main/java/enterprise/Enterprise.java
@@ -0,0 +1,18 @@
+package enterprise;
+
+import behaviours.ITrade;
+import vehicles.Vehicle;
+
+import java.util.ArrayList;
+
+public abstract class Enterprise implements ITrade {
+    private String name;
+    private ArrayList<Vehicle> vehicles;
+    private double cash;
+
+    public Enterprise(String name, ArrayList<Vehicle> vehicles, double cash) {
+        this.name = name;
+        this.vehicles = vehicles;
+        this.cash = cash;
+    }
+}

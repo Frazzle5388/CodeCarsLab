1d3e020d7dbe790bb91c534629e55efe3151d217
Fraser Dillon <fraserdillon050388@gmail.com>
2022-02-15 15:25:46 +0000

    Car test getMake

--- src/test/java/vehicles/CarTest.java
+++ src/test/java/vehicles/CarTest.java
@@ -1,4 +1,22 @@
 package vehicles;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class CarTest {
+
+    Car car;
+
+    @Before
+
+    public void setUp() {
+        car = new Car("Toyota", "Good Car", "Petrol", "Navy Black", 15000.00);
+    }
+
+    @Test
+    public void getMake() {
+        assertEquals("Toyota", car.getMake());
+    }
 }

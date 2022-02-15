4aeb0d392d75cb3615ba621872bd2ef02cdf0a87
Fraser Dillon <fraserdillon050388@gmail.com>
2022-02-15 16:14:48 +0000

    Setting up Dealership tests

--- src/test/java/enterprise/DealershipTest.java
+++ src/test/java/enterprise/DealershipTest.java
@@ -1,4 +1,24 @@
 package enterprise;
 
+import org.junit.Before;
+import vehicles.Car;
+import vehicles.ElectricCar;
+import vehicles.HybridCar;
+import vehicles.Vehicle;
+
 public class DealershipTest {
+
+    Dealership dealership;
+    Car car;
+    ElectricCar electricCar;
+    HybridCar hybridCar;
+
+    @Before
+    public void setUp() {
+        dealership = new Dealership();
+        car = new Car("Toyota", "Good Car", "Petrol", "Navy Black", 15000.00);
+        electricCar = new ElectricCar("Mercedes", "Bad Car", "Electric", "Black", 75000.00);
+        hybridCar = new HybridCar("Lada", "Okay Car", "Petrol", "Navy", 1500.00);
+    }
+
 }

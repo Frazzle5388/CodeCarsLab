42c8e6ebf2bfb922ac25ccf604d49047c2e9962c
Fraser Dillon <fraserdillon050388@gmail.com>
2022-02-15 15:12:05 +0000

    Vehicle attributes, contructer and getters initialised

--- src/main/java/vehicles/Vehicle.java
+++ src/main/java/vehicles/Vehicle.java
@@ -1,4 +1,38 @@
 package vehicles;
 
 public abstract class Vehicle {
+
+    private String make;
+    private String model;
+    private String engine;
+    private String colour;
+    private double price;
+
+    public Vehicle(String make, String model, String engine, String colour, double price) {
+        this.make = make;
+        this.model = model;
+        this.engine = engine;
+        this.colour = colour;
+        this.price = price;
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public String getEngine() {
+        return engine;
+    }
+
+    public String getColour() {
+        return colour;
+    }
+
+    public double getPrice() {
+        return price;
+    }
 }
